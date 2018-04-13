6ed45b97538cc6379a98e514e37ea99183f15fa6
Beto <zonanabeto@gmail.com>
2018-04-13 12:54:22 -0500

    Ya tiene musica y pipes

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ function Board(){
     this.img = new Image();
     this.img.src = "http://ellisonleao.github.io/clumsy-bird/data/img/bg.png";
     this.score = 0;
-
+    this.music = new Audio();
+    this.music.src = "assets/Mario and Luigi - Partners in Time - Main Title 2.mp3" 
     
     
     this.img.onload = function(){
@@ -29,10 +30,14 @@ function Board(){
         this.move();
         ctx.drawImage(this.img,this.x,this.y,this.width,this.height);
         ctx.drawImage(this.img,this.x + canvas.width,this.y,this.width,this.height)
-        ctx.font = "50px Avenir";
-        ctx.fillStyle = "peru";    
-        ctx.fillText(this.score,this.width/2,this.y+50);
+        
 
+    }
+    
+    this.drawScore = function(){
+        ctx.font = "50px Avenir";
+        ctx.fillStyle = "white";    
+        ctx.fillText(this.score,this.width/2,this.y+50);
     }
 }
 
@@ -62,28 +67,67 @@ function Flappy(){
 
 }
 
+//pipes
+
+function Pipe(y,height){
+    this.x = canvas.width;
+    this.y = y;
+    this.width = 50;
+    this.height=height;
+
+    this.draw = function(){
+        this.x --;
+        ctx.fillStyle = "green"; 
+        ctx.fillRect(this.x,this.y,this.width,this.height);
+    }
+
+
+}
+
 
 //declaraciones
 
 var tablero = new Board();
 var flappy = new Flappy();
+var pipes =[];
+
 
 var intervalo;
 var frames = 0;
 
+//aux function
+function generatePipes(){
+        if(!(frames % 200 === 0))return;
+        var ventana = 100;
+        var randomHeight = Math.floor(Math.random()* 200 ) + 50;
+        var pipe = new Pipe(0,randomHeight);
+        var pipe2 = new Pipe(randomHeight+ventana ,canvas.height-(randomHeight+ventana));
+        pipes.push(pipe);
+        pipes.push(pipe2);
+}
+
+function drawPipes(){
+    pipes.forEach(function(pipe){
+        pipe.draw();
+    })
+}
+
 //main function
 
 function update(){
+    generatePipes();
     frames ++;
     console.log(frames);
     ctx.clearRect(0,0,canvas.width,canvas.height);
     tablero.draw();
     flappy.draw();
+    drawPipes();
+    tablero.drawScore();
 }
 
 function start(){
     //extras que necesitemos inicializar
-
+    tablero.music.play();
     if(intervalo>0)return;
     intervalo = setInterval(function(){
     update();  
@@ -91,6 +135,7 @@ function start(){
 }
 
 function stop(){
+    tablero.music.pause();
     clearInterval(intervalo);
     intervalo=0;
 }
